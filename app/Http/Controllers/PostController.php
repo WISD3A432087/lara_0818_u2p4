26de0787d6097e7af0678bacac1c413de6e3aa27
s3A432087 陳人傑 <h220641963@gmail.com>
2017-11-24 20:29:22 +0800

    設定 Controller 回傳對應的 view

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+   public function index(){
+       return view('index');
+   }
+
+    public function show(){
+        return view('post');
+    }
+
+    public function about(){
+        return view('about');
+    }
+
+    public function contact(){
+        return view('contact');
+    }
+}
